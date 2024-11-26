8a58cc8b00d81791c196cc39f2692ac5494711f7
Jason Andrada <andrada.jasonp@gmail.com>
2024-11-26 12:38:43 -0500

    handle incoming form data

--- mental_health_backend/main.py
+++ mental_health_backend/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .models.journal import Journal
+
+import logging
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -10,7 +29,11 @@ def read_root():
     }
 
 @app.post("/journal")
-def send_response():
+def handle_entry(journal: Journal):
+
+    logging.warning("======================")
+    logging.warning(journal)
+    logging.warning("======================")
     return {
         "response": "You're doing great, keep going. Things will get better, I promise"
     }
