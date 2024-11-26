177f92a926290ed603960d7877d7886a9afc0e57
Jason Andrada <andrada.jasonp@gmail.com>
2024-11-26 16:23:32 -0500

    update styles

--- mental_health_frontend/src/components/Nav.jsx
+++ mental_health_frontend/src/components/Nav.jsx
@@ -1,14 +1,18 @@
 import { Link, useLocation } from 'react-router-dom';
+import TopBar from "./TopBar.jsx";
 
 export default function Nav() {
     const location = useLocation();
 
     return (
-        <div className="navigation">
-            <nav>
-                <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Today</Link> | </span>
-                <span><Link className={`nav-link ${location.pathname == "/historical" ? 'current' : ""}`} to="/historical">Past Entries</Link></span>
-            </nav>
+        <div className="combined-top">
+            <TopBar />
+            <div className="navigation">
+                <nav>
+                    <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Today</Link> | </span>
+                    <span><Link className={`nav-link ${location.pathname == "/historical" ? 'current' : ""}`} to="/historical">Past Entries</Link></span>
+                </nav>
+            </div>
         </div>
     )
 }
