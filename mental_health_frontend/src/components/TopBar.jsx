d97b33768f34d5f953f249d7058d27128a13af3f
Jason Andrada <andrada.jasonp@gmail.com>
2024-11-26 16:23:12 -0500

    create topbar component

---
+++ mental_health_frontend/src/components/TopBar.jsx
@@ -0,0 +1,8 @@
+export default function TopBar() {
+    return (
+        <div className="top-bar">
+            <h3>Mental Health Journaling</h3>
+            <h4>You are welcome here</h4>
+        </div>
+    )
+}
