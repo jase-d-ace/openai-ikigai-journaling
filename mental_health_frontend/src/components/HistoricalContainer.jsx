39e0306fb3c1d5c2cbb4c12009139ffb6963b3a2
Jason Andrada <andrada.jasonp@gmail.com>
2024-11-27 11:26:09 -0500

    build out historical view

--- mental_health_frontend/src/components/HistoricalContainer.jsx
+++ mental_health_frontend/src/components/HistoricalContainer.jsx
@@ -1,9 +1,20 @@
 export default function HistoricalContainer() {
+    const pastEntries = JSON.parse(localStorage.getItem("past_entries")) || [];
     return (
         <div>
-            <span>
-                coming soon!
-            </span>
+            {
+                pastEntries.length > 0 ?
+                pastEntries.map(entry => (
+                    <div>
+                        <h1>{entry.date}</h1>
+                        {entry.title}
+                        {entry.feeling}
+                        {entry.journal_entry}
+                        {entry.response}
+                    </div>
+                )) : 
+                <span> No entries yet! Go make some. </span>
+            }
         </div>
     )
 }
