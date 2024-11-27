7ad817554b2c134660be89413f66b90f2438e98f
Jason Andrada <andrada.jasonp@gmail.com>
2024-11-27 16:31:06 -0500

    start styling for mobile

--- mental_health_frontend/src/App.jsx
+++ mental_health_frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         const json = await data.json()
         setResults(json.response)
 
-        let pastEntries = JSON.parse(localStorage.getItem("past_entries")) || []
+        let pastEntries = JSON.parse(localStorage.getItem("past_entries")) || [];
 
         const newEntry = {
             ...formData,
@@ -44,36 +44,47 @@ function App() {
                 <h2>
                     Today's Journal Entry
                 </h2>
-                <h3>
+                <h4>
                     Tell us how you're feeling, then write some notes, and hear something uplifting from GPT :)
-                </h3>
+                </h4>
             </header>
-            <form onSubmit={handleFormSubmit}>
-                <input type="text" placeholder="title" onChange={e => debounce("title", e.target.value, 500)} />
-                <div className="feeling-gauge">
-                    {
-                        [
-                            {feeling: -2, label: ":((", emotion: "very sad"},
-                            {feeling: -1, label: ":(", emotion: "sad"},
-                            {feeling: 0, label: ":|", emotion: "neutral or numb"},
-                            {feeling: 1, label: ":)", emotion: "happy"},
-                            {feeling: 2, label: ":))", emotion: "very happy"},
-                        ].map((option, i) => 
-                        <div key={i} className="option">
-                            <label>{option.label}</label>
-                            <input 
-                                type="radio" 
-                                aria-label={option.emotion} 
-                                onChange={() => setFormData({...formData, feeling: option.feeling})} 
-                                value={option.feeling}
-                                checked={formData.feeling == option.feeling}
-                            />                        
-                        </div>
-                        )
-                    }
+            <form className="journal-form" onSubmit={handleFormSubmit}>
+                <div className="form-part title">
+                    <label>Title</label>
+                    <input className="text-input" required type="text" placeholder="title" onChange={e => debounce("title", e.target.value, 500)} />
                 </div>
-                <textarea onChange={e => debounce("journal_entry", e.target.value, 500)} rows="10" cols="100"></textarea>
-                <input type="submit" value="gogogo" />
+                <div className="form-part feelings">
+                    <label>How are you feeling?</label>
+                    <div className="feeling-gauge">
+                        {
+                            [
+                                {feeling: -2, label: ":(("},
+                                {feeling: -1, label: ":("},
+                                {feeling: 0, label: ":|"},
+                                {feeling: 1, label: ":)"},
+                                {feeling: 2, label: ":))"},
+                            ].map((option, i) => 
+                            <div key={i} className="option">
+                                <label>{option.label}</label>
+                                <input 
+                                    required
+                                    className="submit"
+                                    type="radio" 
+                                    aria-label={option.emotion} 
+                                    onChange={() => setFormData({...formData, feeling: option.feeling})} 
+                                    value={option.feeling}
+                                    checked={formData.feeling == option.feeling}
+                                />                        
+                            </div>
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="form-part journal-entry">
+                    <label>Write your thoughts</label>
+                    <textarea className="text-input" required onChange={e => debounce("journal_entry", e.target.value, 500)} rows="10" cols="100"></textarea>
+                </div>
+                <input className="submit" type="submit" value="gogogo" />
             </form>
 
             <div className="results-container">
