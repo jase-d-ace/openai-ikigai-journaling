54e28dded21fcb41ce4323fbeb533aaa12673eb7
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-10 15:26:13 -0500

    check if isLoggedIn

--- mental_health_frontend/src/components/Nav.jsx
+++ mental_health_frontend/src/components/Nav.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../authContext.jsx";
 import { Link, useLocation } from 'react-router-dom';
 
 export default function Nav() {
     const location = useLocation();
+    const context = useContext(AuthContext)
 
     return (
-        <div className="navigation">
-            <nav>
-                <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Today</Link> | </span>
-                <span><Link className={`nav-link ${location.pathname == "/historical" ? 'current' : ""}`} to="/historical">Past Entries</Link></span>
-            </nav>
-        </div>
+        context.currentUser.isLoggedIn &&
+            <div className="navigation">
+                <nav>
+                    <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Today</Link> | </span>
+                    <span><Link className={`nav-link ${location.pathname == "/historical" ? 'current' : ""}`} to="/historical">Past Entries</Link></span>
+                </nav>
+            </div>
     )
 }
