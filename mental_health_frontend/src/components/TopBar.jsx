16eecfe9a8619cf8540e1ba32ceb9928b71a1bd9
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-10 16:50:07 -0500

    add login route

--- mental_health_frontend/src/components/TopBar.jsx
+++ mental_health_frontend/src/components/TopBar.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../authContext";
+
 export default function TopBar() {
+    const context = useContext(AuthContext);
+
     return (
         <div className="top-bar">
             <h3>Mental Health Journaling</h3>
-            <h4>You are welcome here</h4>
+            {context.isLoggedIn ? <h4>You are welcome here</h4> : <h4><Link to="/login">Login</Link></h4>}
         </div>
     )
 }
--- mental_health_frontend/src/main.jsx
+++ mental_health_frontend/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import { AuthProvider } from './authContext.jsx';
+import { AuthProvider } from "./authContext.jsx";
 import App from './App.jsx'
 import Nav from "./components/Nav.jsx";
 import HistoricalContainer from './components/HistoricalContainer.jsx';
+import Login from "./components/Login.jsx";
 import Footer from "./components/Footer.jsx";
 import TopBar from './components/TopBar.jsx';
 
@@ -18,6 +19,7 @@ createRoot(document.getElementById('root')).render(
                 <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/historical" element={<HistoricalContainer />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
