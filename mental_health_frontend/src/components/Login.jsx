b2f50575cd235db0c29bb30cee952b8473f6c192
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-10 17:16:04 -0500

    create login and register components

--- mental_health_frontend/src/components/Login.jsx
+++ mental_health_frontend/src/components/Login.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
 import { AuthContext } from "../authContext";
 
 export default function Login() {
     return (
-        <h1>Login Form Here</h1>
+        <div className="login">
+            <h1>Log in to start journaling</h1>
+
+            <form>
+                <label>Username</label>
+                <input type="text" />
+                <label>Password</label>
+                <input type="password" />
+                <input type="submit" />
+            </form>
+
+            <span>Don't have an account? <Link to="/register">Create one</Link></span>
+        </div>
     )
 
 }
---
+++ mental_health_frontend/src/components/Register.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Register() {
+    return (
+        <div class="register">
+            <h1>Create an Account</h1>
+            <form>
+                <label>username</label>
+                <input type="text" />
+                <label>password</label>
+                <input type="password" />
+                <label>Confirm password</label>
+                <input type="password" />
+
+                <input type="submit" />
+            </form>
+
+            <span>Already have an account? <Link to="/login">Log In!</Link></span>
+        </div>
+    )
+}
