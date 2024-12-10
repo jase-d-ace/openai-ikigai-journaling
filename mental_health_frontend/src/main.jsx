8497dc35623571531992dc1f2ed3f74c2b7ddc91
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-10 13:46:40 -0500

    wrap app in context provider

--- mental_health_frontend/src/main.jsx
+++ mental_health_frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
+import { AuthProvider } from './authContext.jsx';
 import App from './App.jsx'
 import Nav from "./components/Nav.jsx";
 import HistoricalContainer from './components/HistoricalContainer.jsx';
@@ -10,14 +11,16 @@ import TopBar from './components/TopBar.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <TopBar />
-            <Nav />
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/historical" element={<HistoricalContainer />} />
-            </Routes>
-            <Footer />
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <TopBar />
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/historical" element={<HistoricalContainer />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </AuthProvider>
     </StrictMode>
 )
