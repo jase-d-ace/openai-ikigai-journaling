e9ecda636f8895e2249a3761d1328e272d449963
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-12 12:37:09 -0500

    create registration flow

--- mental_health_frontend/src/authContext.jsx
+++ mental_health_frontend/src/authContext.jsx
@@ -3,14 +3,16 @@ import { createContext, useState, useContext } from "react";
 const AuthContext = createContext({
     user: "",
     isLoggedIn: false,
-    token: ""
+    token: "",
+    error: "Something went wrong"
 });
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState({
-        user: {},
+        user: "",
         isLoggedIn: false,
-        token: ""
+        token: "",
+        error: ""
     });
 
     const login = async (username, password) => {
@@ -39,7 +41,8 @@ export const AuthProvider = ({ children }) => {
             setCurrentUser({
                 user: "",
                 isLoggedIn: false,
-                token: ""
+                token: "",
+                error: json.message
             })
         }
     }
@@ -48,12 +51,55 @@ export const AuthProvider = ({ children }) => {
         setCurrentUser({
             user: "",
             isLoggedIn: false,
-            token: ""
+            token: "",
+            error: ""
         });
+        localStorage.removeItem("user_token");
+    }
+
+    const register = async (username, password, confirm) => {
+        if (password !== confirm) {
+            setCurrentUser({
+                user: "",
+                isLoggedIn: false,
+                token: "",
+                error: "Passwords must match"
+            })
+        }
+
+        const res = await fetch("http://localhost:8000/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+
+        const json = await res.json();
+
+        if (json.status == 200) {
+            setCurrentUser({
+                user: json.user,
+                token: json.access_token,
+                isLoggedIn: true
+            })
+
+            localStorage.setItem("user_token", json.access_token)
+        } else {
+            setCurrentUser({
+                user: "",
+                isLoggedIn: false,
+                token: "",
+                error: json.message
+            })
+        }
     }
 
     return (
-        <AuthContext.Provider value={{ currentUser, login, logout }}>
+        <AuthContext.Provider value={{ currentUser, login, logout, register }}>
             {children}
         </AuthContext.Provider>
     )
--- mental_health_frontend/src/components/Register.jsx
+++ mental_health_frontend/src/components/Register.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
+import { useAuth } from "../authContext";
 import { Link } from "react-router-dom";
 
 export default function Register() {
+    const { register } = useAuth();
+    const [registerFormData, setRegisterFormData] = useState({
+        username: "",
+        password: "",
+        confirm: ""
+    })
+
+    const handleFormInput = (name, text) => {
+        setRegisterFormData({...registerFormData, [name]: text})
+    }
+
+    const handleFormSubmit = async e => {
+        e.preventDefault();
+        await register(registerFormData.username, registerFormData.password, registerFormData.confirm)
+
+    }
+
     return (
         <div className="register">
             <h1>Create an Account</h1>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <label>username</label>
-                <input type="text" />
+                <input onChange={e => handleFormInput("username", e.target.value)} type="text" />
                 <label>password</label>
-                <input type="password" />
+                <input onChange={e => handleFormInput("password", e.target.value)} type="password" />
                 <label>Confirm password</label>
-                <input type="password" />
+                <input onChange={e => handleFormInput("confirm", e.target.value)} type="password" />
 
                 <input type="submit" />
             </form>
