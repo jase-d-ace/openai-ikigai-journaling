f0a66a10773da5db3e554cdcbc9681673733dd80
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-12 22:02:23 -0500

    create journal entry component

---
+++ mental_health_frontend/src/components/JournalEntry.jsx
@@ -0,0 +1,27 @@
+import "../App.css";
+
+export default function JournalEntry({ published_at, title, feeling, content, answer}) {
+    const feelingsMap = {
+        "-2": "Awful",
+        "-1": "Bad",
+        "0": "Neutral",
+        "1": "Good",
+        "2": "Amazing"
+
+    }
+    return (
+        <div className="journal-entry">
+            <div className="journal-header">
+                <strong>{ new Date(published_at).toLocaleDateString() } - {title} </strong>
+                <p className="emotion"> You felt <strong>{feelingsMap[feeling]}</strong> </p>
+            </div>
+            <div className="entry">
+                <span>{content}</span>                
+            </div>
+            <div className="answer">
+                <strong> GPT-4o-mini said... </strong>
+                <span>{answer}</span>
+            </div>
+        </div>
+    )
+}
