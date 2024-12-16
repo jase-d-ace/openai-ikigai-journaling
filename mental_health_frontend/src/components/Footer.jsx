0adcb39c813747db8e8756cabf122314bca25a3f
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-16 18:05:24 -0500

    remove footer component

--- mental_health_frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="footer">
-            <footer>
-                Remember, there is always hope. If you feel yourself drowning, reach out and ask for help. You are not alone. National Alliance on Mental Illness (NAMI) HelpLine: 1-800-950-6264 
-            </footer>
-        </div>
-    )
-}
