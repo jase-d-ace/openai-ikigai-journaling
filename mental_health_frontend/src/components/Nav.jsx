e03d4f56f1976c6739fbff919ee4c08dd022d3ba
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-16 15:27:28 -0500

    flip between nav bar and title

--- mental_health_frontend/src/components/Nav.jsx
+++ mental_health_frontend/src/components/Nav.jsx
@@ -1,15 +1,20 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useAuth } from "../authContext";
 
 export default function Nav() {
     const location = useLocation();
+    const { currentUser } = useAuth();
 
     return (
-        localStorage.getItem("user_token") &&
+        currentUser.isLoggedIn ? 
             <div className="navigation">
                 <nav>
                     <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Today</Link> | </span>
                     <span><Link className={`nav-link ${location.pathname == "/historical" ? 'current' : ""}`} to="/historical">Past Entries</Link></span>
                 </nav>
-            </div>
+            </div> : 
+            <header className="hero-image">
+                <h1>Welcome to your Mental Health Tracker</h1>
+            </header>
     )
 }
