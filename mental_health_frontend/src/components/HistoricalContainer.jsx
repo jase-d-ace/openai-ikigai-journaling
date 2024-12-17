41b6aeb6fbf91dd049696a03ee4a2180c7b45bc2
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-17 14:30:24 -0500

    remove old quote that is no longer relevant

--- mental_health_frontend/src/components/HistoricalContainer.jsx
+++ mental_health_frontend/src/components/HistoricalContainer.jsx
@@ -33,7 +33,6 @@ export default function HistoricalContainer() {
                     <span> No entries yet! Go make some. </span>
                 }                
             </div>
-            <p>Just remember. No matter what anyone tells you, and this includes yourself, you are a positive addition to this world. No one should hold power over you to make you feel any way you don't want to feel.</p>
         </div> :
         <Navigate replace to="/login" />
     )
