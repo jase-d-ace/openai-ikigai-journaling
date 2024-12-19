e286784ddedb010a471197e6e93e7804ab4c46f4
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-19 18:15:18 -0500

    move questions to own file

---
+++ mental_health_frontend/src/questions.js
@@ -0,0 +1,47 @@
+export const questions = [
+    {
+        label: "passion",
+        title: "Your Passion",
+        human_label: "What Do You Love?",
+        placeholder: "Are you absorbed in your work? Do you have a hobby or craft you can’t seem to get enough of? Are you more excited about your hobby or craft than anything else?",
+        response: "",
+        description: "This is what brings you joy. Think about all of the things in your life, whether they are \"productive\" or not, and make note of them here. These are your hobbies, activities that make you genuinely happy, and things that make you feel alive",
+        questions: ["What activities make you lose track of time?", "What hobbies, interests, or topics make you feel energized or excited?", "When was the last time you truly felt happy or fulfilled? What were you doing?", "If money weren't a factor, how would you spend your days?"]
+    },
+    {
+        label: "profession",
+        title: "Your Profession",
+        human_label: "What Are You Good At?",
+        placeholder: "Do people ask you for advice on topics related to your work? Are you/Do you want to be an expert at what you do? Do people compliment you on your hobby or craft?",
+        response: "",
+        description: "This is about your skills and abilities. Think about things you excel at or have the potential to master through practice and discipline. Make note of activities that come naturally to you or your innate talents. Think about things that people ask you for help with.",
+        questions: ["What skills or talents do people often compliment you on?", "What have you been told you're naturally gifted at?", "What do you excel at that others find challenging?", "If you had to teach someone a skill or subject, what would it be?"]
+    },
+    {
+        label: "mission",
+        title: "Your Mission",
+        human_label: "What Does the World Need?",
+        placeholder: "Is your work considered a high demand in the marketplace? Picture the next year, 10 years, and 100 years — will your work still be valuable? Are you solving a social, economic, or environmental problem?",
+        response: "",
+        description: "This focuses on how your actions or contributions can create a positive impact. Don't worry about changing the whole world, but think about how you can maybe use your skills and abilities to affect positive change in your community or inner circle. Think about the problems that you care about: envirnmental issues, social justice causes, or even just something as easy as helping a friend. Your mission is where your passions meet an unmet need in your world. It's about answering \"How can I help?\"",
+        questions: ["What problems in the world are you passionate about solving?", "What challenges are you emotionally connected to? Climate change? Education? Mental health?", "If you could change one thing about this world, what would it be?", "How do you want to be remembered?", "If you could volunteer or donate to any nonprofit or charity organization, what causes would they champion?"]
+    },
+    {
+        label: "vocation",
+        title: "Your Vocation",
+        human_label: "What Can You Be Paid For?",
+        placeholder: "Do you make a good living/Will you eventually make a good living doing your work? Have other people made a career out of the same hobby or craft?",
+        response: "",
+        description: "This is where you get practical. Think about aligning your skills or passion with opportunities that provide financial stability. This is where you think about monetizing the skills and activities that make you feel fulfilled. Think about your marketable skills; skills that you have or can develop that people would pay for and find a way to align those skills with what others see as valuable.",
+        questions: ["What skills or expertise do you currently have that others already pay for?", "What industries offer opportunities to apply these skills?","What are some examples of people earning money doing work you'd also like to do?", "Are there hobbies or side projects that you enjoy that could potentially generate income?"]
+    },
+    {
+        label: "content",
+        title: "Your Own Thoughts",
+        human_label: "Other Thoughts (Optional)",
+        placeholder: "What else is on your mind that's relevant to what you love and what you believe your calling is?",
+        response: "",
+        description: "Feel free to leave this blank. But any notes you add here will be taken into account as part of GPT's analysis of your answers.",
+        questions: ["What else do you feel is non-negotiable to your core being?", "What are values that you hold dear to your own heart?", "Are there things that don't fall into any of these buckets that still give you a reason to live?"]
+    }
+];
