a204471898b71dee8ce646912de615b869f270ee
Jason Andrada <andrada.jasonp@gmail.com>
2024-12-20 14:23:15 -0500

    include About page in routing

--- mental_health_frontend/src/components/Nav.jsx
+++ mental_health_frontend/src/components/Nav.jsx
@@ -9,6 +9,7 @@ export default function Nav() {
         currentUser.isLoggedIn ? 
             <div className="navigation">
                 <nav>
+                    <span><Link className={`nav-link ${location.pathname == "/what-is" ? 'current' : ""}`} to="/what-is"> About </Link> | </span>
                     <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Today</Link> | </span>
                     <span><Link className={`nav-link ${location.pathname == "/historical" ? 'current' : ""}`} to="/historical">Past Entries</Link></span>
                 </nav>
--- mental_health_frontend/src/main.jsx
+++ mental_health_frontend/src/main.jsx
@@ -9,6 +9,7 @@ import HistoricalContainer from './components/HistoricalContainer.jsx';
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import TopBar from './components/TopBar.jsx';
+import About from "./components/About.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
                     <Route path="/historical" element={<HistoricalContainer />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/what-is" element={<About />} />
                 </Routes>
             </BrowserRouter>
         </AuthProvider>
