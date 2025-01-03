9d7b2350342660ae7826a139bddc0a5eb4ae6b53
Jason Andrada <andrada.jasonp@gmail.com>
2025-01-03 12:01:25 -0500

    add copy to footer and render on page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import TopBar from './components/TopBar.jsx';
 import About from "./components/About.jsx";
+import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -24,6 +25,7 @@ createRoot(document.getElementById('root')).render(
                     <Route path="/register" element={<Register />} />
                     <Route path="/what-is" element={<About />} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
         </AuthProvider>
     </StrictMode>
