c49d1f851e6414261f0a8a26bae96cf431d8677a
Jason Andrada <andrada.jasonp@gmail.com>
2025-01-03 12:47:46 -0500

    stylings and some UX language

--- frontend/src/components/FormNav.jsx
+++ frontend/src/components/FormNav.jsx
@@ -1,9 +1,10 @@
 export default function FormNav( { questions, currentQuestion, setCurrentQuestion } ) {
+    const responseNeeded = question => !question.response;
     return (
         <div className="form-nav navigation">
             {
                 questions.map((question, i) => (
-                <span className={`${currentQuestion == i ? "current" : ""}`} onClick={() => setCurrentQuestion(i)}> {question.label} </span>))
+                <span className={`${currentQuestion == i ? "current" : ""} ${responseNeeded(question) ? "unanswered" : ""}`} onClick={() => setCurrentQuestion(i)}> {question.label} </span>))
             }
         </div>
     )
