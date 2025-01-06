43fd31d0a4926bd38a8e84926d84a9ca8dd8ad5a
Jason Andrada <andrada.jasonp@gmail.com>
2025-01-06 15:24:57 -0500

    cleanup

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Your Calling is entirely your own. Be your best self by leaning into what brings you the most joy.</p>
+        </footer>
+    )
+}
