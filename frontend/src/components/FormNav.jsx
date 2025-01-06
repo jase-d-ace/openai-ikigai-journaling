ed4583ac583f8caff9ae1ca06ec3ee051869e84e
Jason Andrada <andrada.jasonp@gmail.com>
2025-01-06 14:00:02 -0500

    capitalize form nav bar

--- frontend/src/components/FormNav.jsx
+++ frontend/src/components/FormNav.jsx
@@ -1,11 +1,18 @@
+const capitalize = str => {
+    const [first, ...rest] = str.split("");
+    const capital = first.toUpperCase();
+    return `${capital}${rest.join("")}`;
+};
+
 export default function FormNav( { questions, currentQuestion, setCurrentQuestion } ) {
     const responseNeeded = question => !question.response;
+
     return (
         <div className="form-nav navigation">
             {
                 questions.map((question, i) => (
-                <span className={`${currentQuestion == i ? "current" : ""} ${responseNeeded(question) ? "unanswered" : ""}`} onClick={() => setCurrentQuestion(i)}> {question.label} </span>))
+                <span className={`${currentQuestion == i ? "current" : ""} ${responseNeeded(question) ? "unanswered" : ""}`} onClick={() => setCurrentQuestion(i)}> {capitalize(question.label)} </span>))
             }
         </div>
-    )
-}
+    );
+};
