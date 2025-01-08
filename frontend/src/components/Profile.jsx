088a459e6e927c935740688361c5698e69d20887
Jason Andrada <andrada.jasonp@gmail.com>
2025-01-08 13:53:48 -0500

    continue fleshing out profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../authContext.jsx";
+import Loading from "../components/Loading.jsx";
 import "../App.css";
 
 export default function Profile() {
@@ -12,15 +13,39 @@ export default function Profile() {
             <div className="profile-container">              
                 <div className="personal-info">
                     <div className="profile-section my-info">
-                        my info
+                        <header>
+                            <h4>My Info</h4>
+                        </header>
+                        <div className="section-info">
+                            <div className="info-field">
+                                <label htmlFor="first-name">First Name: </label>
+                                <input className="profile-input-field name" value={currentUser.first_name || ""} disabled={!isEditing} />
+                            </div>
+                            <div className="info-field">
+                                <label htmlFor="last-name">Last Name: </label>
+                                <input className="profile-input-field name" value={currentUser.last_name || ""} disabled={!isEditing} />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="profile-info">
                     <div className="profile-section description">
-                        description
+                        <header>
+                            <h4>Who I Am</h4>
+                        </header>
+                        <div className="section-info">
+                            <div className="info-field">
+                                <textarea rows="10" cols="49" value={currentUser.description || ""} disabled={!isEditing} />
+                            </div>
+                        </div>
                     </div>
                     <div className="profile-section gpt-analysis">
-                        gpt-analysis
+                        <header>
+                            <h4>What GPT thinks of me</h4>
+                        </header>
+                        <div className="section-info">
+                            <Loading />
+                        </div>
                     </div>
                 </div>
             </div>
