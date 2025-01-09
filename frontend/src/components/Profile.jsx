69a117c59993333b532da053b11d9689795973c9
Jason Andrada <andrada.jasonp@gmail.com>
2025-01-09 11:47:30 -0500

    continue fleshing out profile functionality

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,10 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../authContext.jsx";
 import Loading from "../components/Loading.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import Markdown from "react-markdown";
 import "../App.css";
 
 export default function Profile() {
@@ -15,21 +16,29 @@ export default function Profile() {
         firstName: "",
         lastName: "",
         description: "",
-    })
+    });
+    const [loading, setLoading] = useState(false);
+
+    const [gptResponse, setGptResponse] = useState("");
+
+    useEffect(() => {
+        if (isEditing.name) {
+            nameRef.current.select();
+        }
+
+        if (isEditing.description) {
+            descriptionRef.current.select();
+        }
+    }, [isEditing])
     const { currentUser, updateUserInfo } = useAuth();
     const nameRef = useRef();
     const descriptionRef = useRef();
 
-    const handleClick = (field, ref) => {
+    const handleClick = (ref) => {
         setIsEditing({
             ...isEditing,
             [ref]: !isEditing[ref]
         })
-
-        if (isEditing[ref]) {
-            console.log(field)
-            field.current.focus();
-        }
     }
 
     const handleInputChange = (name, query) => {
@@ -41,18 +50,32 @@ export default function Profile() {
 
     const handleProfileFormChange = async () => {
         const { firstName, lastName, description} = profileFormData
-        await updateUserInfo(currentUser.id, firstName, lastName, description)
+        await updateUserInfo(currentUser.user.id, localStorage.getItem("user_token"), firstName, lastName, description)
+        setIsEditing({
+            name: false,
+            description: false
+        })
+    }
+
+    const fetchGPTResponse = async () => {
+        setLoading(true);
+
+        const gptRes = await fetch(`http://localhost:8000/users/generate-gpt?id=${currentUser.user.id}`);
+        const gptJson = await gptRes.json();
+
+        setGptResponse(gptJson.analysis);
+        setLoading(false);
     }
 
     return (
         <div className="my-profile">
-            <h1>Me</h1>
+            <h2>My Profile</h2>
             <div className="profile-container">              
                 <div className="personal-info">
                     <div className="profile-section my-info">
                         <header>
                             <h4>My Info</h4>
-                            <FontAwesomeIcon icon={faEdit} className="edit-button" onClick={() => handleClick(nameRef, "name")} />
+                            <FontAwesomeIcon icon={faEdit} className="edit-button" onClick={() => handleClick("name")} />
                         </header>
                         <div className="section-info personal-inputs">
                             <div className="info-field">
@@ -68,7 +91,7 @@ export default function Profile() {
                     <div className="profile-section description">
                         <header>
                             <h4>Who I Am</h4>
-                            <FontAwesomeIcon icon={faEdit} className="edit-button" onClick={() => handleClick(descriptionRef, "description")} />
+                            <FontAwesomeIcon icon={faEdit} className="edit-button" onClick={() => handleClick("description")} />
                         </header>
                         <div className="section-info">
                             <div className="info-field">
@@ -76,15 +99,28 @@ export default function Profile() {
                             </div>
                         </div>
                     </div>
-                    {(isEditing.name || isEditing.description) && <button className="button profile-button" onClick={() => handleProfileFormChange()}>Save</button>}
+                    <div className="profile-section journal-count">
+                        <header>
+                            <h4>Ikigai Journeys Taken</h4>
+                        </header>
+                        <div className="section-info">
+                            <div className="info-field">
+                                <label htmlFor="journal-count">Total Journals: </label>
+                                <input className="profile-input-field name" disabled value={currentUser.user.journal_count} />
+                            </div>
+                        </div>
+                    </div>
+                    {(isEditing.name || isEditing.description) && <button className="profile-button" onClick={() => handleProfileFormChange()}>Save</button>}
                 </div>
                 <div className="profile-info">
                     <div className="profile-section gpt-analysis">
                         <header>
                             <h4>What GPT thinks of me</h4>
                         </header>
-                        <div className="section-info">
-                            <Loading />
+                        <div className="section-info gpt-field">
+                            {loading && <Loading />}
+                            {gptResponse && <Markdown>{gptResponse}</Markdown>}
+                            {!gptResponse && <button className="profile-button" onClick={() => fetchGPTResponse()}>Get Graded</button>}
                         </div>
                     </div>
                 </div>
